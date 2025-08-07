07c0c3884d3f7d0973a116e4a107b7394915407a
Olaniyi Mercy <olaniyimercy45@gmail.com>
2025-08-07 10:33:48 +0100

    done

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+const getUser = async () => {
+    fetch('https://fakestoreapi.com/products/')
+    // Change data from API to JSON
+    .then((result) => result.json())
+    .then((data) => 
+        {
+            const allProducts = document.getElementById('products');
+            data.forEach(product => {
+               let eachProduct = document.createElement('div')
+               eachProduct.setAttribute('id', 'home')
+               eachProduct.innerHTML = `
+               <h2>${product.title}</h2>
+               <img src="${product.image}"/>
+               <p>${product.description}</p>
+               `
+            allProducts.appendChild(eachProduct)
+            });
+        })
+    .catch((err) => console.log("Error fetching data", err));
+}
+getUser()
